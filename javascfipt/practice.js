8003dd4df37cb91c1f29d782cdb20ae536fdb23a
KAMAL-IFET <139547312+KAMAL-IFET@users.noreply.github.com>
2024-03-27 12:29:01 +0530

    Add files via upload

---
+++ javascfipt/practice.js
@@ -0,0 +1,15 @@
+let arr=['Dhoni','virat','rohit','sachin']
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+console.log(arr.push('bumrah'))
+console.log(arr.push('gaikwad'))
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+console.log(arr.indexOf('sachin'));
+
