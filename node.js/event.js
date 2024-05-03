0373de28b5e6c684602399c1f87e9895250fadbe
KAMAL-IFET <139547312+KAMAL-IFET@users.noreply.github.com>
2024-05-03 07:30:25 +0530

    event.js

--- node.js/event.js
+++ node.js/event.js
@@ -1,3 +1,13 @@
+<!-- Title: practice node
+-- Author: B.KAMAL
+-- Created Date: 01.05.2024
+-- Last modified Date: 02.05.2024
+-- Reviewed By:
+-- Reviewed Date:  -->
+
+
+
+
 var EventEmitter = require('events');
 
 var emitter = new EventEmitter();
